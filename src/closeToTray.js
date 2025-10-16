1b0bba532f7ae6affb0dda8844b46917b6149cf7
Michał Szopiński <mszopinski@mssnt.pl>
2025-10-17 01:56:07 +0200

    Reduce nested code

--- src/closeToTray.js
+++ src/closeToTray.js
@@ -2,44 +2,46 @@ const { ExtensionCommon } = ChromeUtils.importESModule("resource://gre/modules/E
 
 const activeWindows = new Set();
 
-class CloseToTray extends ExtensionCommon.ExtensionAPI {
-    getAPI(context) {
-        function registerWindow(windowId) {
-            if (activeWindows.has(windowId))
-                return;
+function registerWindow(context, windowId) {
+    if (activeWindows.has(windowId))
+        return;
 
-            const window = context.extension.windowManager.get(windowId, context).window;
-            const baseWindow = window.docShell.treeOwner.QueryInterface(Ci.nsIBaseWindow);
+    const window = context.extension.windowManager.get(windowId, context).window;
+    const baseWindow = window.docShell.treeOwner.QueryInterface(Ci.nsIBaseWindow);
 
-            function handleClose(event) {
-                // only hide Thunderbird when there are no other main windows
-                if (activeWindows.size > 1) {
-                    activeWindows.delete(windowId);
+    function handleClose(event) {
+        // only hide Thunderbird when there are no other main windows
+        if (activeWindows.size > 1) {
+            activeWindows.delete(windowId);
 
-                    window.closeToTrayClose();
-                    return;
-                }
-
-                event?.preventDefault();
-
-                // minimize required, otherwise the window will be restored (un-maximized) when the tray icon is clicked
-                window.minimize();
-
-                Cc["@mozilla.org/messenger/osintegration;1"].getService(Ci.nsIMessengerWindowsIntegration).hideWindow(baseWindow);
-            }
-
-            // handle close from taskbar
-            window.addEventListener("close", handleClose);
-
-            // handle close from X button
-            window.closeToTrayClose = window.close;
-            window.close = handleClose;
-
-            activeWindows.add(windowId);
+            window.closeToTrayClose();
+            return;
         }
 
+        event?.preventDefault();
+
+        // minimize required, otherwise the window will be restored (un-maximized) when the tray icon is clicked
+        window.minimize();
+
+        Cc["@mozilla.org/messenger/osintegration;1"].getService(Ci.nsIMessengerWindowsIntegration).hideWindow(baseWindow);
+    }
+
+    // handle close from taskbar
+    window.addEventListener("close", handleClose);
+
+    // handle close from X button
+    window.closeToTrayClose = window.close;
+    window.close = handleClose;
+
+    activeWindows.add(windowId);
+}
+
+class CloseToTray extends ExtensionCommon.ExtensionAPI {
+    getAPI(context) {
         return {
-            closeToTray: { registerWindow }
+            closeToTray: {
+                registerWindow: registerWindow.bind(null, context)
+            }
         };
     }
 };
