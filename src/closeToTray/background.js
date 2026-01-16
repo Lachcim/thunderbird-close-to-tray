41225373ec5f18ec337a612d6080865f53ac295b
Michał Szopiński <mszopinski@mssnt.pl>
2026-01-16 14:24:37 +0100

    Move error handler to separate file

--- src/closeToTray/background.js
+++ src/closeToTray/background.js
@@ -5,45 +5,5 @@ function handleWindow(window) {
     }
 }
 
-let errorWindowId = null;
-let errorWindowParams = null;
-
-async function getErrorWindow(error) {
-    if (errorWindowId == null)
-        return { window: null, valid: false };
-
-    try {
-        const window = await browser.windows.get(errorWindowId);
-        return { window, valid: JSON.stringify(error) == errorWindowParams };
-    }
-    catch {
-        return { window: null, valid: false };
-    }
-}
-
-async function handleFailure(error) {
-    const { window, valid } = await getErrorWindow(error);
-
-    if (window && valid) {
-        browser.windows.update(window.id, { focused: true });
-        return;
-    }
-
-    if (window)
-        await browser.windows.remove(window.id);
-
-    const newWindow = await browser.windows.create({
-        type: "popup",
-        url: `ui/error.html?${new URLSearchParams(error)}`,
-        width: 440,
-        height: 540
-    });
-
-    errorWindowId = newWindow.id;
-    errorWindowParams = JSON.stringify(error);
-}
-
 messenger.windows.onCreated.addListener(handleWindow);
-messenger.closeToTray.onFail.addListener(handleFailure);
-
 messenger.windows.getAll().then(openWindows => openWindows.forEach(handleWindow));
